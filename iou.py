fe914ab454be61b24452d8c079cd1cac11287a99
Jiayi Lu <215756347@qq.com>
2022-08-25 14:15:34 +0800

    Update iou.py

--- iou.py
+++ iou.py
@@ -103,3 +103,30 @@ jaccard(torch.tensor(pred), torch.tensor(mask))
 # if the union is zero, then it has to be an OK image AND prediction is an OK image !
 
 ##################################################################
+
+
+
+pred = np.array([
+    [0, 0, 0],
+    [0, 1, 1],
+    [0, 0, 1]
+])
+
+mask = np.array([
+    [0, 0, 1],
+    [0, 1, 1],
+    [0, 0, 0]
+])
+
+compute(pred, mask)
+
+compute(1-pred, 1-mask)
+
+jaccard(torch.tensor(pred), torch.tensor(mask))
+
+#{'intersection': 2, 'union': 4, 'iou': 0.5}
+
+#{'intersection': 5, 'union': 7, 'iou': 0.7142857142857143}
+
+# 5 = 9 - 4
+# 7 = 9 - 2
